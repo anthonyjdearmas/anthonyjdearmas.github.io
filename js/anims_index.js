2bbddc7f0dd4b13e1bfed9240c4135a07ef8e6d2
ajd249 <45925337+ajd249@users.noreply.github.com>
2020-05-10 13:11:21 -0400

    Uploading website

---
+++ js/anims_index.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+  
+  var scrollLink = $('.scroll');
+  
+  scrollLink.click(function(e) {
+    e.preventDefault();
+    $('body,html').animate({
+      scrollTop: $(this.hash).offset().top
+    }, 1200 );
+  });
+  
+  $(window).scroll(function() {
+    var scrollbarLocation = $(this).scrollTop();
+    
+    scrollLink.each(function() {
+      
+      if (scrollbarLocation >= 400) {
+        $('.about').parent().addClass('active');
+        $('.home').parent().removeClass('active');
+      } else {
+        $('.about').parent().removeClass('active');
+        $('.home').parent().addClass('active');
+      }
+    })
+  })
+})
+
+$(document).ready(function() {
+
+  $(".about").click(function(){
+    var $this = $(this);
+    if($this.data('clicked')) {
+        console.log("CLICKED ABOUT IS TRUE")
+        this.preventDefault();
+        window.location.href = 'index.html';
+        $([document.documentElement, document.body]).animate({
+          scrollTop: $(".about").offset().top
+      }, 2000);
+    }
+  });
+})
